44c5d38eff4abfff80f50835d9480c110a921552
zbaldino-cloud <zbaldino@shrewsbury.k12.ma.us>
2026-09-22 13:40:14 -0400

    Add Swift Package test harness

---
+++ Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "AppSOFACore",
+    platforms: [
+        .macOS(.v13)
+    ],
+    products: [
+        .library(name: "AppSOFACore", targets: ["AppSOFACore"])
+    ],
+    targets: [
+        .target(
+            name: "AppSOFACore",
+            path: "Client/AppSOFA",
+            exclude: [
+                "AppSOFA.swift",
+                "Models/ApplicationRequirement.swift",
+                "Models/ChromeSecurityRelease.swift",
+                "Models/InstalledApplication.swift",
+                "Models/SecurityInformation.swift",
+                "Services/ApplicationScanner.swift",
+                "Services/ChromeVersionService.swift",
+                "Services/CISAKEVService.swift",
+                "Services/FeedLoader.swift",
+                "Services/SecuritySLAService.swift"
+            ],
+            sources: [
+                "Services/FeedFreshnessService.swift",
+                "Services/VersonComparator.swift"
+            ]
+        ),
+        .testTarget(
+            name: "AppSOFACoreTests",
+            dependencies: ["AppSOFACore"],
+            path: "Client/AppSOFATests"
+        )
+    ]
+)
