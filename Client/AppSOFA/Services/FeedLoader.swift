9fd97b7852177cba5e5c7a25ff57110ddcb9af7f
zbaldino-cloud <zbaldino@shrewsbury.k12.ma.us>
2026-09-22 13:52:36 -0400

    Verify feed signature before decoding

--- Client/AppSOFA/Services/FeedLoader.swift
+++ Client/AppSOFA/Services/FeedLoader.swift
@@ -5,8 +5,26 @@ enum FeedLoader {
         string: "https://zbaldino-cloud.github.io/APPSofa/v1/macos_apps_data_feed.json"
     )!
 
+    static let signatureURL = URL(
+        string: "https://zbaldino-cloud.github.io/APPSofa/v1/macos_apps_data_feed.json.sig"
+    )!
+
     static func loadRemoteFeed() async throws -> AppFeed {
-        var request = URLRequest(url: feedURL)
+        async let feedDownload = download(feedURL)
+        async let signatureDownload = download(signatureURL)
+
+        let (feedData, signatureData) = try await (feedDownload, signatureDownload)
+
+        try FeedSignatureVerifier.verify(
+            feedData: feedData,
+            signatureData: signatureData
+        )
+
+        return try JSONDecoder().decode(AppFeed.self, from: feedData)
+    }
+
+    private static func download(_ url: URL) async throws -> Data {
+        var request = URLRequest(url: url)
         request.cachePolicy = .reloadIgnoringLocalCacheData
         request.timeoutInterval = 30
 
@@ -17,7 +35,7 @@ enum FeedLoader {
             throw FeedError.invalidResponse
         }
 
-        return try JSONDecoder().decode(AppFeed.self, from: data)
+        return data
     }
 }
 
